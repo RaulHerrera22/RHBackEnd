cb9b194525c0e358992aee8dfc9d20a77fa3bf0b
Raul <shaulihot@gmail.com>
2023-01-15 17:55:24 -0300

    Creacion carpeta FrontEnd
    se puso todo en nueva carpeta FrontEnd

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienciaLComponent } from './components/experiencia-l/experiencia-l.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardComponent } from './components/hard/hard.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent,
+    BannerComponent,
+    AboutMeComponent,
+    ExperienciaLComponent,
+    EducacionComponent,
+    HardComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
