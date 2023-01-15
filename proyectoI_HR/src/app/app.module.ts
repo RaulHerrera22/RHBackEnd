a4d999266351337d8c2b5d7c1ec76c71329bd668
Raul <shaulihot@gmail.com>
2023-01-15 08:07:41 -0300

    header/aboutME
    header completado y creacion de componente about me

--- proyectoI_HR/src/app/app.module.ts
+++ proyectoI_HR/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { RSocialComponent } from './components/r-social/r-social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent,
+    RSocialComponent,
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
